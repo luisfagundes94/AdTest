94561cb4c3c8f59f39faa31aa6ef1ded975f02cc
Luis Felipe Carvalho Fagundes <luis.fagundes@al.infnet.edu.br>
2020-03-05 17:37:31 -0300

    implementing admob

--- app/src/main/java/com/example/adtest/MainActivity.kt
+++ app/src/main/java/com/example/adtest/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.adtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val AD_KEY = "ca-app-pub-3940256099942544~3347511713"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }}
+
+        MobileAds.initialize(this, AD_KEY)
+
+        val adRequest = AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .build()
+
+        adView.loadAd(adRequest)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adView.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        adView.destroy()
+    }
+}
+
